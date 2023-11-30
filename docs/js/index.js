948b7a44a00c5e013176ce8ed466105d47af6df8
mujlax <zx053a@yandex.ru>
2023-11-30 12:51:00 +0300

    dev

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,13 +1,62 @@
 let rows = [];
 let rowsDefault;
 
+let sliceNumber = 5;
+let pageNumber = 0;
+let pageNumber2 = 5;
+function pagination(value) {
+    rows = rowsDefault.slice(pageNumber, pageNumber2);
+    renderTable(rows);
+    if (value == "left") {
+        pageNumber -= sliceNumber;
+        pageNumber2 -= sliceNumber;
+        renderTable(rows);
+        console.log(pageNumber);
+        return
+    }
+    console.log(value);
+    pageNumber += sliceNumber;
+    pageNumber2 += sliceNumber;
+    console.log(pageNumber);
+    renderTable(rows);
+}
+
+let step = 5;
+let page = 0;
+function setStep(value) {
+    step = Number(value);
+    pageRender(0)
+    //console.log(pag);
+}
+function inc(){
+    if (page*step+step < rowsDefault.length)
+        page++;
+    pageRender(page)
+}
+
+function dec(){
+    if (page != 0)
+        page--;
+    pageRender(page)
+}
+
+function pageRender(pageNum){
+    let index = pageNum*step;
+    rows = rowsDefault.slice(index, index+step);
+    console.log({page, step, index, rows})
+    renderTable(rows);
+}
+
+
 function renderTable(rows) {
     const table = document.querySelector("table");
     const tableHead = table.querySelector("thead");
     const tableBody = table.querySelector("tbody");
 
     tableBody.innerHTML = "<tr></tr>";
-
+    const counter = document.createElement("h1");
+    counter.textContent = rows.length;
+    tableBody.appendChild(counter);
     for (const row of rows) {
         const rowElement = document.createElement("tr");
 
@@ -53,8 +102,8 @@ async function loadIntoTable(url, table) {
     //Clear
 
     tableBody.innerHTML = "<tr></tr>";
-
-    renderTable(rows)
+    
+    pageRender(0)
 }
 
 async function loadData(url) {
@@ -102,7 +151,7 @@ function test3(str) {
 
 function searchUser(obj) {
     // поиск по email или имени
-    const newRows = rows.filter((el) => 
+    const newRows = rowsDefault.filter((el) => 
         // поиск по email 
         el.email.toLowerCase().includes(obj.value.toLowerCase()) || el.username.toLowerCase().includes(obj.value.toLowerCase())
     )
@@ -111,10 +160,15 @@ function searchUser(obj) {
 }
 
 function deleteUser(id) {
-    rows = rows.filter((el) => el.id != id); 
-    rowsDefault = rowsDefault.filter((el) => el.id != id); 
-    console.log(rows);
-    renderTable(rows);
+    let isDelete = confirm("ТЫ РИЛ ХОЧЕШЬ УДАЛИТЬ ЧЕЛА???")
+    if (isDelete){
+        rows = rows.filter((el) => el.id != id); 
+        rowsDefault = rowsDefault.filter((el) => el.id != id); 
+        console.log(rows);
+        renderTable(rows);
+    } else {
+        alert("Ну ок, не удаляем")
+    }
     // поиск по email или имени
     // const newRows = rows.filter((el) => 
     //     el.email.toLowerCase().includes(obj.value.toLowerCase()) || el.username.toLowerCase().includes(obj.value.toLowerCase())
@@ -125,8 +179,9 @@ function deleteUser(id) {
 
 function remove() {
     document.querySelector(".search__input").value = "";
-    renderTable(rowsDefault);
+    renderTable(rows);
 }
 
 console.log(rows);
 //document.querySelector("table").innerHTML = "<tr></tr>"
+
